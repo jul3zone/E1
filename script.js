a4f7dafb20d91057e136ace53c845447e8b0bdd4
Jules J <63829550+jul3zone@users.noreply.github.com>
2020-05-21 19:20:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-var lis = document.querySelectorAll("li")
-var done = document.querySelectorAll("li").classList.toggle("done");
+var list = document.getElementsByTagName("li")
+// var done = document.querySelectorAll("li").classList.toggle("done");
+
 
 function inputLength() {
 	return input.value.length;
@@ -15,22 +16,16 @@ function createListElement() {
 	input.value = "";
 }
 
-function addDoneClass() {
-	done.addEventListener("click"), function() {
-		console.log();
-	}
+
+// here we go, this is the right way. thanks stack overflow.
+function toggleDone() {
+	this.classList.toggle("done");
 }
 
-function createDeleteButton() {
-	var done = document.createElement("button");
-	// li.appendChild(document.create(input.value));
-	ul.appendChild(li);
-	// input.value = "";
+for (var i = 0; i < list.length; i++) {
+	list[i].addEventListener("click", toggleDone);
 }
 
-// function addDoneClass() {
-// 	li.
-// }
 
 function addListAfterClick() {
 	if (inputLength() > 0) {
@@ -45,8 +40,12 @@ function addListAfterKeypress(event) {
 }
 
 
+
+
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
 
-lis.addEventListener("click", classList.toggle = "done");
+// lis.addEventListener("click", classList.toggle("done"));
+
+// document.querySelectorAll("li").classList.toggle("done")
